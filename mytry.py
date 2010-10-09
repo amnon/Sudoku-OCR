f4c80791631813b6dcfca23180ba69f756db1011
amnon <none@none>
2010-10-09 22:51:14 +0200

    begin to implement algorithm described in "iPhone Sudoku Grab: How does it all work?"

--- mytry.py
+++ mytry.py
@@ -14,9 +14,9 @@ def binarizeImage(im):
     # zeroing the others.
     # the image may not be illuminated uniformly, so apply an adaptive
     # threshold.
-    divisor = 9
-    blockSize = min(cv.GetSize(im)) / divisor
-    blockSize += 1 - blockSize % 2 # make sure it's odd
+    # actually, OpenCV's adaptive threshold is less than perfect because param1
+    # isn't relative to the average pixel values.
+    blockSize = 9
     bin = cv.CloneImage(im)
     cv.AdaptiveThreshold(im, bin, 255, thresholdType=cv.CV_THRESH_BINARY_INV,
         blockSize=blockSize)
@@ -36,16 +36,27 @@ def main():
     im = cv.LoadImage(filename, cv.CV_LOAD_IMAGE_GRAYSCALE)
     showImage("B&W", im)
     
-    # smooth a little since image is noisy (at least from Nokia...)
-    smoothed = cv.CloneImage(im)
-    cv.Smooth(im, smoothed, cv.CV_MEDIAN, 5)
-    im = smoothed
-    showImage("Smoothed", im)
+    # smooth a little since image is noisy (at least with Nokia...)
+    if True:
+        smoothed = cv.CloneImage(im)
+        cv.Smooth(im, smoothed, cv.CV_MEDIAN, 5)
+        im = smoothed
+        showImage("Smoothed", im)
+    
+    # downscale
+    # (for compatibility with iPhone Soduko Grab. but maybe
+    # it will help to always work on the same size?)
+    imSize = cv.GetSize(im)
+    tmp = cv.CreateImage((imSize[0]/3, imSize[1]/3), im.depth, 1)
+    cv.Resize(im, tmp)
+    im = tmp
     
     # convert to binary
     im = binarizeImage(im)
     showImage("Binary", im)
 
+    # # scale down image 
+    
     cv.WaitKey(0)
     
 if __name__ == "__main__":
